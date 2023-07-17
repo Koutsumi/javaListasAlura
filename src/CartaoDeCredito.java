2b28b30153e8cb2c9525d17c5d129807f3bee20a
Koutsumi <febaccarini@gmail.com>
2023-07-17 19:49:44 -0300

    desafio

--- src/CartaoDeCredito.java
+++ src/CartaoDeCredito.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class CartaoDeCredito {
+import Compra.Compra;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private double limite;
+    private double saldo;
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+
+    private List<Compra> compras;
+
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public boolean lancaCompra(Compra compra){
+        if(this.saldo >= compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import Compra.Compra;
+
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Qual o valor limite do cartão?");
+        double limite = leitura.nextDouble();
+        CartaoDeCredito cartao = new CartaoDeCredito(limite);
+
+        int sair = 1;
+        while(sair != 0){
+            System.out.println("Digite sua compra:");
+            String descricao = leitura.next();
+
+            System.out.println("Digite o valor da compra:");
+            double valor = leitura.nextDouble();
+
+            Compra compra = new Compra(descricao, valor);
+            boolean compraRealizada = cartao.lancaCompra(compra);
+
+            if(compraRealizada){
+                System.out.println("Compra realizada com sucesso!");
+                System.out.println("Digite 1 para sair ou 0 para continuar:");
+
+                sair = leitura.nextInt();
+            }else {
+                System.out.println("Saldo insuficiente.");
+                sair = 0;
+            }
+
+            System.out.println("Compra realizada:");
+            Collections.sort(cartao.getCompras());
+            for(Compra c : cartao.getCompras()){
+                System.out.println(c.getDescricao() + " - " + c.getValor());
+            }
+            System.out.println("Saldo do cartão:" + cartao.getSaldo());
+        }
     }
 }
